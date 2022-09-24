d271e26ae8f2b8dffc12e8b4ab64991e6d1ddfb9
Eduardo <42901658+LopezSennin@users.noreply.github.com>
2022-09-24 17:27:58 -0500

    main
    listo
    
    Co-Authored-By: Lau <52580838+laura-hmz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PruebasIndependencia import *
 from PruebasUnformidad import *
 from MiGenerador import *
 
-def main(a=0,c=0,m=0,x0=0,generator='GLC', chiCuadrado=False, kolmogorov=False, corridas=False, series=False, poker=False):
+def main(a=0,c=0,m=0,x0=0,generator='GLC',activar_chiCuadrado=True, activar_kolmogorov=True, activar_corridas=True, activar_series=True, activar_poker=True):
     RELACION=[]
     FRECUENCIA=[]
     SECUENCIA=[] #lo del generador hasta la semilla 
@@ -18,17 +18,18 @@ def main(a=0,c=0,m=0,x0=0,generator='GLC', chiCuadrado=False, kolmogorov=False, 
         print('Generador no soportado o erroneo')
         return 
     if len(RELACION):
-        if chiCuadrado:
+        if activar_chiCuadrado:
             PCC(SECUENCIA,FRECUENCIA)
-        elif kolmogorov:
+        if activar_kolmogorov:
             kolmogorov(SECUENCIA,FRECUENCIA)
-        elif corridas:
-            corridas(SECUENCIA,FRECUENCIA)
-        elif series:
+        if activar_corridas:
+            corridas(SECUENCIA)
+        if activar_series:
             series(SECUENCIA)
-        elif poker:
+        if activar_poker:
             poker(SECUENCIA,3)
             poker(SECUENCIA,5)
     else: print('generador retorno lista vacia')
 
-main(a=255, generator='GEM', c=100,m=1033,x0=5)
+#main(a=255, generator='GLC', c=100,m=1033)
+#main(a=255, generator='GEM', c=100,m=1033,x0=5,activar_chiCuadrado=False, activar_kolmogorov=False, activar_corridas=False, activar_series=False, activar_poker=False)
